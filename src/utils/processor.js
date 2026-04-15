6215bc595bb738b954965209ea021d23d2f3f301
igle_code <emilymaze12@gmail.com>
2026-04-15 17:50:06 +0100

    rename count to sample_size

--- src/utils/processor.js
+++ src/utils/processor.js
@@ -3,14 +3,15 @@ function processingGenderData( name, apiData){
 
     const { gender, probability,count}= apiData;
 
-    const is_confident= probability >= 0.7 && count >=100;
+    const sample_size  = count
+    const is_confident= probability >= 0.7 && sample_size >=100;
     const processed_at = new Date().toISOString();
 
     return{
         name,
         gender,
         probability,
-        sample_size: count,
+        sample_size,
         is_confident,
         processed_at,
     };
